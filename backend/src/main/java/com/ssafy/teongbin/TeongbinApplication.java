c3b9c55acdf4d5ae40b95272587e9aa61ce0f47c
황우성 <asdf1916@naver.com>
2024-07-22 13:46:49 +0900

    initial

---
+++ backend/src/main/java/com/ssafy/teongbin/TeongbinApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.teongbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeongbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeongbinApplication.class, args);
+	}
+
+}
