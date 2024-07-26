18e280fca11d234ed4dd851ae52109092a85d35d
오혜성 <evilparan@gmail.com>
2024-07-26 15:46:26 +0900

    feat: 네이버맵 추가

--- frontend/dashboard/src/dashboard/components/home/NaverMap.vue
+++ frontend/dashboard/src/dashboard/components/home/NaverMap.vue
@@ -1,11 +1,34 @@
 <script setup>
+import { onMounted } from "vue";
 
+function initMap() {
+  const mapOptions = {
+    center: new naver.maps.LatLng(35.20389, 126.8069),
+    zoom: 15,
+  };
+
+  map.value = new naver.maps.Map("map", mapOptions);
+}
+
+onMounted(() => {
+  initMap();
+});
 </script>
 
 <template>
-    
+  <section class="map-section">
+    <div id="map" class="map-size"></div>
+  </section>
 </template>
 
 <style scoped>
-
+.map-section {
+  padding: 40px;
+  float: left;
+  padding-right: 1rem;
+}
+.map-size {
+  width: 860px;
+  height: 500px;
+}
 </style>
--- frontend/dashboard/src/dashboard/views/HomeView.vue
+++ frontend/dashboard/src/dashboard/views/HomeView.vue
@@ -1,13 +1,14 @@
 <script setup>
 import DashboardLayout from "@/dashboard/layouts/DashboardLayout.vue";
+import NaverMap from "@/dashboard/components/home/NaverMap.vue";
 </script>
 
 <template>
-    <DashboardLayout>
-
-    </DashboardLayout>
+  <DashboardLayout>
+    <div class="info-container">
+      <NaverMap />
+    </div>
+  </DashboardLayout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
