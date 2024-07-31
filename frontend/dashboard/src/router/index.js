1572c339b06ebab442cf90b7b35d71abec38af47
황우성 <asdf1916@naver.com>
2024-07-31 16:30:12 +0900

    Frontend

--- frontend/dashboard/src/router/index.js
+++ frontend/dashboard/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/auth/stores/auth';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,19 +22,40 @@ const router = createRouter({
     {
       path: '/user/login',
       name: 'login',
-      component: () => import('@/auth/views/LoginView.vue')
+      component: () => import('@/auth/views/LoginView.vue'),
     },
     {
       path: '/user/join',
       name: 'join',
-      component: () => import('@/auth/views/SignupView.vue')
+      component: () => import('@/auth/views/SignupView.vue'),
     },
     {
       path: '/user/find',
       name: 'find',
-      component: () => import('@/auth/views/FindPwdView.vue')
+      component: () => import('@/auth/views/FindPwdView.vue'),
     }
   ]
-})
+});
 
-export default router
+router.beforeEach((from, to, next) => {
+  const token = sessionStorage.getItem("teongbinToken");
+
+  console.log(from);
+  const allowPages = ['login', 'join', 'find'];
+  
+  if(token) {
+    const authStore = useAuthStore();
+    authStore.loginState = true;
+    if(allowPages.includes(from.name)) {
+      return next({name: 'home'});
+    }
+    return next();
+  } else {
+    if(allowPages.includes(from.name)) {
+      return next();
+    }
+    next({name: 'login'});
+  }
+});
+
+export default router;
