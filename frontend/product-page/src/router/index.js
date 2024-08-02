56a05fcffe946efdfe06c905925b85361e4f10db
지예찬 <blackhaze529@naver.com>
2024-08-02 17:58:03 +0900

    feat: 임시

--- frontend/product-page/src/router/index.js
+++ frontend/product-page/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,8 +8,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    }
+      component: HomeView
+    },
   ]
 })
 
