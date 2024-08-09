839f8f5021dd8feb5550b0bbe2d3f03b4e31789e
지예찬 <blackhaze529@naver.com>
2024-08-09 11:36:10 +0900

    refactor: 상품 소개 페이지 글자 중앙

--- frontend/product-page/src/components/ArticleComponent.vue
+++ frontend/product-page/src/components/ArticleComponent.vue
@@ -2,16 +2,17 @@
     <div class="row justify-content-center">
 
         <!-- left -->
-        <article class="container" data-aos="fade-left" data-aos-duration="700" data-aos-once="false" style="padding-top: 0px;">
+        <article class="container" data-aos="fade-left" data-aos-duration="700" data-aos-once="false"
+            style="padding-top: 20px; margin-bottom: 0px;">
             <div class="row justify-content-between">
-                <div class="col-7">
+                <div class="col-7 align-content-center">
                     <h1 class="my-3 fw-bold">쓰레기통 압축 기능</h1>
 
                     <div class="my-3">
                         <h3 class="mt-4">
                             더 이상 넘치는 쓰레기통은 없습니다 !
                         </h3>
-             
+
                         <h4 class="mt-4">
                             텅빈은 프레스 압축 기능으로
                         </h4>
@@ -20,31 +21,30 @@
                         </h4>
                     </div>
                 </div>
-                <div class="col-5" style="margin: auto 0;">
-                    <img src="../assets/press2.png" alt="home_img" style="width:100%; height: auto;">
+                <div class="col-5" style="margin: 0 0;">
+                    <img src="../assets/press2.png" alt="home_img" style="width:100%; ">
                 </div>
             </div>
         </article>
-        
-        <article class="container justify-content-start"
-        data-aos="fade-left" data-aos-duration="700" data-aos-once="false"
-        >
-        <div class="row justify-content-between">
-            <div class="col-5" style="margin: auto 0;">
-                <img src="../assets/cleaner.webp" alt="home_img" style="width:100%; height: auto;">
-            </div>
-            <div class="col-6">
-                <h1 class="my-3 fw-bold">쓰레기통 잔량 확인 서비스</h1>
-                <br>
-                <div class="my-3">
+
+        <article class="container justify-content-start" data-aos="fade-left" data-aos-duration="700"
+            data-aos-once="false">
+            <div class="row justify-content-between">
+                <div class="col-5" style="margin: auto 0;">
+                    <img src="../assets/cleaner.webp" alt="home_img" style="width:100%; height: auto;">
+                </div>
+                <div class="col-6 align-content-center">
+                    <h1 class="my-3 fw-bold">쓰레기통 잔량 확인 서비스</h1>
+                    <br>
+                    <div class="my-3">
                         <h3 class="mt-4">
                             직접 쓰레기통을 찾아가지 마세요
                         </h3>
-             
+
                         <h4 class="mt-4">
                             언제 어디서든 편리하게
                         </h4>
-       
+
                         <h4>
                             내가 관리하는 쓰레기통의 잔량을 확인하세요
                         </h4>
@@ -52,11 +52,12 @@
                 </div>
             </div>
         </article>
-        
+
         <!-- left -->
-        <article class="container" data-aos="fade-left" data-aos-duration="700" data-aos-once="false" style="padding-top: 0px;">
+        <article class="container" data-aos="fade-left" data-aos-duration="700" data-aos-once="false"
+            style="padding-top: 0px; margin-bottom: 100px;">
             <div class="row justify-content-between">
-                <div class="col-7">
+                <div class="col-7 align-content-center">
                     <h1 class="my-3 fw-bold">AI 분리수거 인식 기능</h1>
                     <br>
                     <div class="my-3">
@@ -78,7 +79,7 @@
                 </div>
             </div>
         </article>
-        
+
 
 
     </div>
@@ -88,49 +89,57 @@
 </script>
 
 <style scoped>
-    h1, h2, h3, h4, h5 {
-        text-wrap: wrap;
-    }
-    article {
-        height: 500px;
-        min-width:500px;
-        max-width:1200px;
-        width:100%;
-        align-items: center;
-        padding-top: 50px;
-        padding-bottom: 50px;
-        /* display: flex; */
-        justify-content: space-between;
-        margin-top: 50px;
-        margin-bottom: 30px;
-        overflow: visible;
-        box-sizing: border-box
-    }
-    article img {
-        width: 370px;
-        max-width: 100%;
-        height: 350px;
-        object-fit: fill;
-        border-radius: 40px;
-    }
-    .container-up {
-        min-height: 750px;
-        max-height: 800px;
-        margin-top: 50px;
-        margin-bottom: 0px;
-        padding-bottom: 50px;        
-    }
-    .container-up img {
-        width: 100%;
-        min-height: 100%;
-        height: 400px;
-        min-width:100%;
-        margin-top: 0px;
-    }
-    .container-up h1,
-    .container-up h2,
-    .container-up h5 {
-        word-wrap: break-word;
-    }
-    
+h1,
+h2,
+h3,
+h4,
+h5 {
+    text-wrap: wrap;
+}
+
+article {
+    height: 500px;
+    min-width: 500px;
+    max-width: 1200px;
+    width: 100%;
+    align-items: center;
+    /* padding-top: 50px;
+        padding-bottom: 50px; */
+    /* display: flex; */
+    justify-content: space-between;
+    margin-top: 70px;
+    margin-bottom: 70px;
+    overflow: visible;
+    box-sizing: border-box
+}
+
+article img {
+    width: 370px;
+    max-width: 100%;
+    height: 350px;
+    object-fit: fill;
+    border-radius: 40px;
+}
+
+.container-up {
+    min-height: 750px;
+    max-height: 800px;
+    margin-top: 50px;
+    margin-bottom: 0px;
+    padding-bottom: 50px;
+}
+
+.container-up img {
+    width: 100%;
+    min-height: 100%;
+    height: 400px;
+    min-width: 100%;
+    margin-top: 0px;
+}
+
+.container-up h1,
+.container-up h2,
+.container-up h5 {
+    word-wrap: break-word;
+}
 </style>
