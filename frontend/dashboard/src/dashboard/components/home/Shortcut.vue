8454a5b9eec8290f057e9c0733a96b7c1c94e42b
오혜성 <evilparan@gmail.com>
2024-08-14 09:24:33 +0900

    fix: shortcut 수정 시 hover 되지 않으면 수정창이 닫히는 문제 해결

--- frontend/dashboard/src/dashboard/components/home/Shortcut.vue
+++ frontend/dashboard/src/dashboard/components/home/Shortcut.vue
@@ -46,10 +46,14 @@ function mouseoverHandler(idx) {
   if (hoverTimeout.value) {
     clearTimeout(hoverTimeout.value);
   }
-  hoverTimeout.value = setTimeout(() => {
+  
+  if (select.value != idx) {
+    hoverTimeout.value = setTimeout(() => {
     renameInputVisible.value = false;
     select.value = idx;
   }, 500);
+  }
+  
 }
 
 function mouseleave() {
